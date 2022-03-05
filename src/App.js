d2237ece867a4b6889779d731a602a60c489f2c5
norbusonam <nsonam1822@gmail.com>
2022-03-05 09:24:52 -0800

    added lorem info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import KnightsTour from './components/KnightsTour';
+import KnightsTourInfo from './components/KnightsTourInfo';
 
 function App() {
   return (
     <div className='container'>
       <KnightsTour />
+      <KnightsTourInfo />
     </div>
   );
 }
---
+++ src/components/KnightsTourInfo.js
@@ -0,0 +1,10 @@
+function KnightsTourInfo() {
+  return (
+    <div>
+      <h2>What is a knight's tour?</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsam sed temporibus molestias eveniet, incidunt cum quisquam impedit tempora dolor illo quam, magni accusantium fugiat odit voluptatem voluptates, iusto porro!</p>
+    </div>
+  )
+}
+
+export default KnightsTourInfo;
