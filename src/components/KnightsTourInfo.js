e074a5f9134060d722b1248bf79b9117134bd112
norbusonam <nsonam1822@gmail.com>
2022-09-27 21:47:05 -0700

    added desciption with source

--- src/components/KnightsTourInfo.js
+++ src/components/KnightsTourInfo.js
@@ -2,7 +2,8 @@ function KnightsTourInfo() {
   return (
     <div style={{ padding: '0 0.5rem' }}>
       <h2>What is a knight's tour?</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsam sed temporibus molestias eveniet, incidunt cum quisquam impedit tempora dolor illo quam, magni accusantium fugiat odit voluptatem voluptates, iusto porro!</p>
+      <p>A knight's tour is a sequence of moves of a knight on a chessboard such that the knight visits every square exactly once. If the knight ends on a square that is one knight's move from the beginning square (so that it could tour the board again immediately, following the same path), the tour is closed (or re-entrant); otherwise, it is open.</p>
+      <small>Source: <a target="_blank" rel="noreferrer" href="https://en.wikipedia.org/wiki/Knight%27s_tour">Wikipedia</a></small>
     </div>
   )
 }
